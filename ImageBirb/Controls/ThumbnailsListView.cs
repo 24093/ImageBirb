c53de7a1486197acebebc49065e8f3a381c57d18
wunderdoge <services@alkl.de>
2019-01-31 22:53:08 +0100

    ThumbnailsListView to custom control

---
+++ ImageBirb/Controls/ThumbnailsListView.cs
@@ -0,0 +1,27 @@
+﻿using ImageBirb.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ImageBirb.Controls
+{
+    internal class ThumbnailsListView : ListView
+    {
+        public static readonly DependencyProperty SelectedImageViewModelProperty = DependencyProperty.Register(
+            "SelectedImageViewModel", typeof(SelectedImageViewModel), typeof(ThumbnailsListView), new PropertyMetadata(default(SelectedImageViewModel)));
+
+        public static readonly DependencyProperty ThumbnailListViewModelProperty = DependencyProperty.Register(
+            "ThumbnailListViewModel", typeof(ThumbnailListViewModel), typeof(ThumbnailsListView), new PropertyMetadata(default(ThumbnailListViewModel)));
+
+        public SelectedImageViewModel SelectedImageViewModel
+        {
+            get => (SelectedImageViewModel)GetValue(SelectedImageViewModelProperty);
+            set => SetValue(SelectedImageViewModelProperty, value);
+        }
+
+        public ThumbnailListViewModel ThumbnailListViewModel
+        {
+            get => (ThumbnailListViewModel)GetValue(ThumbnailListViewModelProperty);
+            set => SetValue(ThumbnailListViewModelProperty, value);
+        }
+    }
+}
