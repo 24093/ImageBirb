3d217dc3d88dff113d3ede68dc4d89890fb64d2e
wunderdoge <services@alkl.de>
2019-01-31 22:22:49 +0100

    TagsListView to custom control

---
+++ ImageBirb/Controls/TagsListView.cs
@@ -0,0 +1,27 @@
+﻿using ImageBirb.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ImageBirb.Controls
+{
+    internal class TagsListView : ListView
+    {
+        public static readonly DependencyProperty TagListViewModelProperty = DependencyProperty.Register(
+            "TagListViewModel", typeof(TagListViewModel), typeof(TagsListView), new PropertyMetadata(default(TagListViewModel)));
+
+        public static readonly DependencyProperty ThumbnailListViewModelProperty = DependencyProperty.Register(
+            "ThumbnailListViewModel", typeof(ThumbnailListViewModel), typeof(TagsListView), new PropertyMetadata(default(ThumbnailListViewModel)));
+
+        public TagListViewModel TagListViewModel
+        {
+            get => (TagListViewModel) GetValue(TagListViewModelProperty);
+            set => SetValue(TagListViewModelProperty, value);
+        }
+        
+        public ThumbnailListViewModel ThumbnailListViewModel
+        {
+            get => (ThumbnailListViewModel) GetValue(ThumbnailListViewModelProperty);
+            set => SetValue(ThumbnailListViewModelProperty, value);
+        }
+    }
+}
